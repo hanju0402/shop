66aae5ed268263432034f39322ad9e065617e344
DESKTOP-F5MR2TE\USER <nahanju0402@naver.com>
2023-12-06 17:59:20 +0900

    shop 프로젝트 react-bootstrap 사용 public 폴더에 이미지 넣어서 사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import "./App.css";
+import { useState } from "react";
+import data from './data.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let [shoes] = useState(data);
+
+    return (
+        <div className="App">
+            <Navbar bg="light" data-bs-theme="light">
+                <Container>
+                    <Navbar.Brand href="#home">HJ.SHOP</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <div className="main-bg"></div>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <img src={process.env.PUBLIC_URL + "/img/shoes1.jpg"} width="80%" />
+                        <h4>상품명</h4>
+                        <p>상품설명</p>
+                    </Col>
+                    <Col>
+                        <img src={process.env.PUBLIC_URL + "/img/shoes2.jpg"} width="80%" />
+                        <h4>상품명</h4>
+                        <p>상품설명</p>
+                    </Col>
+                    <Col>
+                        <img src={process.env.PUBLIC_URL + "/img/shoes3.jpg"} width="80%" />
+                        <h4>상품명</h4>
+                        <p>상품설명</p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
